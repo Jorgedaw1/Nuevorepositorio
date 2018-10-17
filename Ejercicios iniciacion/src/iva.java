6d21d3f84a446e7110969bab1f5a368445f0bdd2
adf_21 <adridialga@hotmail.com>
2018-10-17 12:13:35 +0200

    Ejercicio nuevo: 17/10

--- Ejercicios iniciacion/src/iva.java
+++ Ejercicios iniciacion/src/iva.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
 
 public class iva {
-
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduce un precio: ");
+		float precio = in.nextFloat();
+		System.out.println("Tipo de iva: (0) 7% - (1) 10% - (2) 21%");
+		int tipoIVA = in.nextInt();
+		float iva = 0;
+		if (tipoIVA == 0)
+			iva = 0.07f;
+		else if (tipoIVA == 1)
+			iva = 0.1f;
+		else if (tipoIVA == 2) 
+			iva = 0.21f;
+		else
+			System.out.println("El tipo de IVA no es correcto");
+		
+		if (iva != 0) {
+			double totalIVA = precio * iva;
+			double total = precio + totalIVA;
+			
+		}
+		in.close();
+	}
 }
