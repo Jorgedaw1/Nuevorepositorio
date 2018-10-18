a9c3a984b7d4776806ab8354b3f75e29602b8a69
adf_21 <adridialga@hotmail.com>
2018-10-18 14:24:53 +0200

    EjerciciosDeExamenes

---
+++ Ejercicios iniciacion/src/Examen1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class Examen1 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Número de filas ");
+		int n = in.nextInt();
+		System.out.println("Número de columnas ");
+		int m = in.nextInt();
+//		for (int i=0; i<n; i++) {
+//			if ( i == n / 2) {
+//				for (int j = 0; j<m; j++)
+//					System.out.print("*");
+//			}
+//			else {
+//				for (int j = 0; j<m; j++) {
+//					if (j == m / 2) {
+//						System.out.print("*");
+//					}
+//					else
+//						System.out.print(" ");
+//				}
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<m; j++) {
+			if(i == n/2 || j == m/2) {
+				System.out.print("*");
+			}
+			else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	}
+}
---
+++ Ejercicios iniciacion/src/Examen2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Examen2 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Introduce un número mayor que 1 ");
+		int n = in.nextInt();
+		
+	}
+}
